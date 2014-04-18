b8537dfe7092612a8a4f76f61888415a7ab98c70
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-04-18 01:25:55 -0400

    change require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,8 @@
 
 gem 'minitest'
 
-require 'minitest/spec'
 require 'minitest/autorun'
+require 'minitest/spec'
 
 $:.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
 require 'google_speech'
