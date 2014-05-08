779273cf03e9f3c363ce77697a84f94f52c3501c
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-05-08 15:25:33 -0400

    change spec for new default values

--- spec/transcriber_spec.rb
+++ spec/transcriber_spec.rb
@@ -9,11 +9,10 @@ describe GoogleSpeech::Transcriber do
 
     transcriber = GoogleSpeech::Transcriber.new(f)
     t = transcriber.transcribe
-    # puts "\n\nt: #{t.inspect}\n\n"
 
     t.size.must_equal 2
     t.first[:start_time].must_equal 0
-    t.first[:end_time].must_equal 5.5
+    t.first[:end_time].must_equal 4.25
     t.first[:text].wont_be_nil
     t.first[:confidence].wont_be_nil
   end
